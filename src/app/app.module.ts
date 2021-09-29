369554cfa1958d7cb717fc047b911a929b1f2291
priti gupta <priti.gupta@gslab.com>
2021-09-29 14:02:03 +0530

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompComponent } from './comp/comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompComponent
   ],
   imports: [
     BrowserModule,
